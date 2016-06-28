6096361ab209eba7d58c50021f12f71cd10eb3a5
joalmeid <joao.diogo.almeida@gmail.com>
2016-06-28 11:05:18 +0100

    adding code

---
+++ MyWeather.Droid/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace MyWeather.Droid
+{
+	[Activity (Label = "My Weather", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : FormsAppCompatActivity
+    {
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			
+		    ToolbarResource = Resource.Layout.toolbar;
+		    TabLayoutResource = Resource.Layout.tabs;
+		    base.OnCreate (bundle);
+
+		    Forms.Init(this, bundle);
+            Xamarin.Insights.Initialize(Xamarin.Insights.DebugModeKey, this);
+
+            Xamarin.Forms.Forms.ViewInitialized += (object sender, Xamarin.Forms.ViewInitializedEventArgs e) => {
+                if (!string.IsNullOrWhiteSpace(e.View.AutomationId))
+                {
+                    e.NativeView.ContentDescription = e.View.AutomationId;
+                }
+            };
+
+            LoadApplication(new App());
+		}
+	}
+}
+
+
